641ea4f58b1a177a0242b2ecf76c8e899481691d
Gabrielle Scott <scottgabrielle49@gmail.com>
2023-02-28 10:59:27 -0500

    Exercise4

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 ###
@@ -43,22 +44,26 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
-
+    if form.validate_on_submit():
+         # Get the username and password values from the form.
+         formUsername = form.Username
+         formPassword = form.password
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
 
-        # Gets user id, load into session
-        login_user(user)
+    userQuery = session.query(UserProfiles).filter_by(username=formUsername, password=formPassword).scalars()
+    user = userQuery[0]
+    if check_password_hash(user_profile.password, user.password):
+             # Gets user id, load into session
+             login_user(user)
+             # Remember to flash a message to the user
+             flash("Successful login")
+             return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
-
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
